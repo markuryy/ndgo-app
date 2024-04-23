40508591739870859fb59af2f241d79fe741e7af
mark <mr.catborg@gmail.com>
2024-04-23 01:07:49 -0400

    more linting

--- src/pages/privacy.tsx
+++ src/pages/privacy.tsx
@@ -1,15 +1,14 @@
-import { markdownToHtml } from '../lib/markdownToHtml';
 import { SEO } from '@components/common/seo';
 import { MainContainer } from '@components/home/main-container';
 import { Loading } from '@components/ui/loading';
 import { ProtectedLayout } from '@components/layout/common-layout';
 import { MainLayout } from '@components/layout/main-layout';
+import { markdownToHtml } from '../lib/markdownToHtml';
 import type { ReactElement, ReactNode } from 'react';
 
 export default function PrivacyPolicy({ content }: { content: string }): JSX.Element {
-  if (!content) {
+  if (!content) 
     return <Loading />;
-  }
 
   return (
     <MainContainer>
@@ -19,7 +18,7 @@ export default function PrivacyPolicy({ content }: { content: string }): JSX.Ele
   );
 }
 
-export async function getStaticProps() {
+export async function getStaticProps(): Promise<{ props: { content: string } }> {
   const content = await markdownToHtml('content/privacy-policy.md');
   return {
     props: {
--- src/pages/tos.tsx
+++ src/pages/tos.tsx
@@ -1,25 +1,24 @@
-import { markdownToHtml } from '../lib/markdownToHtml';
 import { SEO } from '@components/common/seo';
 import { MainContainer } from '@components/home/main-container';
 import { Loading } from '@components/ui/loading';
 import { ProtectedLayout } from '@components/layout/common-layout';
 import { MainLayout } from '@components/layout/main-layout';
+import { markdownToHtml } from '../lib/markdownToHtml';
 import type { ReactElement, ReactNode } from 'react';
 
 export default function TermsOfService({ content }: { content: string }): JSX.Element {
-  if (!content) {
+  if (!content) 
     return <Loading />;
-  }
 
   return (
     <MainContainer>
-      <SEO title="Terms of Service / ndgo" />
-      <div className="prose dark:prose-dark max-w-none p-4" dangerouslySetInnerHTML={{ __html: content }} />
+      <SEO title='Terms of Service / ndgo' />
+      <div className='prose dark:prose-dark max-w-none p-4' dangerouslySetInnerHTML={{ __html: content }} />
     </MainContainer>
   );
 }
 
-export async function getStaticProps() {
+export async function getStaticProps(): Promise<{ props: { content: string } }> {
   const content = await markdownToHtml('content/terms-of-service.md');
   return {
     props: {
